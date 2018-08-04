d644dc74c1519f5bea57a3a281d94e096e0a6967
liyu-wang <liyu.wang@oasis.com>
2018-08-05 09:38:16 +1000

    tap a photo collection cell to open the photo

---
+++ Photorama/PhotoInfoViewController.swift
@@ -0,0 +1,51 @@
+//
+//  PhotoInfoViewController.swift
+//  Photorama
+//
+//  Created by Liyu Wang on 4/8/18.
+//  Copyright © 2018 Oasis. All rights reserved.
+//
+
+import UIKit
+
+class PhotoInfoViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    
+    var photo: Photo! {
+        didSet {
+            navigationItem.title = photo.title
+        }
+    }
+    var store: PhotoStore!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        store.fetchImage(for: photo) { (result) -> Void in
+            switch result {
+            case let .success(image):
+                self.imageView.image = image
+            case let .failure(error):
+                print("Error fetching image for photo: \(error)")
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Photorama/PhotosViewController.swift
+++ Photorama/PhotosViewController.swift
@@ -41,15 +41,26 @@ class PhotosViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        
+        switch  segue.identifier {
+        case "showPhoto"?:
+            if let selectedIndexPath = collectionView.indexPathsForSelectedItems?.first {
+                let photo = photoDataSource.photos[selectedIndexPath.row]
+                
+                let destinationVC = segue.destination as! PhotoInfoViewController
+                destinationVC.photo = photo
+                destinationVC.store = store
+            }
+        default:
+            preconditionFailure("Unexpected segue identifier.")
+        }
     }
-    */
 
 }
 
