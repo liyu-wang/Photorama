7cd4fc41e16512882d096faed45a5d57a22bd8c6
liyu-wang <liyu.wang@oasis.com>
2018-08-04 19:52:08 +1000

    collection view dataSource

---
+++ Photorama/PhotoDataSource.swift
@@ -0,0 +1,25 @@
+//
+//  PhotoDataSource.swift
+//  Photorama
+//
+//  Created by Liyu Wang on 4/8/18.
+//  Copyright © 2018 Oasis. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDataSource: NSObject, UICollectionViewDataSource {
+    
+    var photos = [Photo]()
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let identifier = "UICollectionViewCell"
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath)
+        return cell
+    }
+    
+}
